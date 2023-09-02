765dce82bdb6dddd9b7cba06d93fdef0648ae95c
RoshakN <msnooraniz@gmail.com>
2023-09-02 22:26:46 +0330

    feat: Add plus icon for adding notes

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,11 @@
+import { RiAddBoxFill } from "react-icons/ri";
+
 function AddTask() {
-  return <div className="absolute bottom-[10px] right-[15px]">+</div>;
+  return (
+    <div className="absolute bottom-[10px] right-[10px] cursor-pointer hover:text-[#f0ae51]">
+      <RiAddBoxFill size={"1.75rem"} />
+    </div>
+  );
 }
 
 export default AddTask;

d5c005f90c824efc9430f0bdf09add66f2132388
RoshakN <msnooraniz@gmail.com>
2023-09-02 12:47:03 +0330

    style: Change cards styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ import Day from "../components/Day";
 
 function Home() {
   return (
-    <div className="overflow-hidden bg-[#ebe7e1] h-screen">
+    <div className="bg-[#ebe7e1] h-screen">
       <Header />
-      <section className="grid grid-cols-2 gap-3 mx-2 md:grid-cols-4">
+      <section className="grid grid-cols-2 mx-2 gap-x-3 md:grid-cols-4">
         <Day day={"Saturday"} />
         <Day day={"Sunday"} />
         <Day day={"Monday"} />

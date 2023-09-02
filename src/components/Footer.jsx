21f676e471342b4f8a1eb33847ca5dd3d968fb27
RoshakN <msnooraniz@gmail.com>
2023-09-02 12:55:23 +0330

    style: Change Footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 function Footer() {
-  return <div className="absolute bottom-0">Footer</div>;
+  return (
+    <div className="sticky bottom-0 text-sm text-center text-white bg-orange-800 h-max">
+      <p className="py-2">Expense Management</p>
+    </div>
+  );
 }
 
 export default Footer;

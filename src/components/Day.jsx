ec4db3b7d3f93991d29f38f91e29db05e2bcd858
RoshakN <msnooraniz@gmail.com>
2023-09-02 22:06:13 +0330

    style: Fix task list margin on bigger screens

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -2,11 +2,11 @@ import AddTask from "./AddTask";
 
 function Day(props) {
   return (
-    <div className="bg-[#fcf8f0] h-[200px] rounded-md relative my-2 md:h-[300px]">
+    <div className="bg-[#fcf8f0] h-[200px] rounded-md relative my-2 md:h-[300px] shadow-md">
       <h1 className="h-max text-[#d4993b] font-bold text-lg text-center rounded-t-md bg-gradient-to-br from-[#fce0b3] to-[#f5e2c4]">
         {props.day}
       </h1>
-      <section className="m-3 text-base list-decimal">
+      <section className="p-3 mx-2 text-base list-decimal">
         <li>task1</li>
         <li>task2</li>
       </section>

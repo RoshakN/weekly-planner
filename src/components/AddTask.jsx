fe78d813cd17b15c5bc02d2e8721b34e6fc5da53
RoshakN <msnooraniz@gmail.com>
2023-09-03 20:55:52 +0330

    style: fix active styling for addTask btn on mobile phones

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -2,7 +2,7 @@ import { RiAddBoxFill } from "react-icons/ri";
 
 function AddTask() {
   return (
-    <div className="absolute bottom-[10px] right-[10px] cursor-pointer hover:text-[#f0ae51]">
+    <div className="absolute bottom-[10px] right-[10px] cursor-pointer active:text-[#f0ae51] md:hover:text-[#f0ae51] md:active:text-[#b6833c]">
       <RiAddBoxFill size={"1.75rem"} />
     </div>
   );

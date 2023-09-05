18abda41b54f9f566b3712ea9b26a7f14611685a
RoshakN <msnooraniz@gmail.com>
2023-09-05 20:51:56 +0330

    feat: Add task modal to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
 import Day from "../components/Day";
+import AddTaskModal from "../components/AddTaskModal";
 // import Footer from "../components/Footer";
 
 function Home() {
   return (
     <div className="overflow-auto bg-[#ebe7e1] h-full md:h-screen">
+      <AddTaskModal />
       <Header />
       <section className="grid grid-cols-2 mx-2 gap-x-3 md:grid-cols-4">
         <Day day={"Saturday"} />

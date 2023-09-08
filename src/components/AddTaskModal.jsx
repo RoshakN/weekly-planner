8527aa194185612e8d2cf9f807b06af27bf2fd7b
RoshakN <msnooraniz@gmail.com>
2023-09-08 09:58:35 +0330

    feat: Add new task modal

---
+++ src/components/AddTaskModal.jsx
@@ -0,0 +1,66 @@
+import { useSelector, useDispatch } from "react-redux";
+import { modalActions } from "../store/modal";
+import { planActions } from "../store/plan";
+
+import { RiCloseCircleFill } from "react-icons/ri";
+import { Fragment } from "react";
+
+function AddTaskModal() {
+  const modalState = useSelector((state) => state.modal);
+  const planState = useSelector((state) => state.plan);
+
+  const dispatch = useDispatch();
+
+  const addHandler = () => {
+    dispatch(planActions.addTaskSat([...planState.saturday, modalState.text]));
+    dispatch(modalActions.setOpen(false));
+  };
+
+  const closeHandler = () => {
+    dispatch(modalActions.setOpen(false));
+  };
+
+  const handleChange = () => {
+    dispatch(modalActions.setText(event.target.value));
+  };
+
+  return (
+    <Fragment>
+      {modalState.open && (
+        <div className="z-50 bg-[#fcf8f0] h-screen w-screen fixed flex justify-center items-center">
+          <div className="w-1/2 bg-gradient-to-br from-[#fce0b3] to-[#f5e2c4] h-1/3 flex flex-col items-center rounded-lg">
+            <div className="flex font-greatVibes w-full h-max items-center bg-gradient-to-br from-[#f8d49a] to-[#f1b95f] rounded-t-lg">
+              <h1 className="p-3 text-xl">Create a new task</h1>
+              <RiCloseCircleFill
+                className="m-3 ml-auto cursor-pointer hover:text-[#efefef]"
+                onClick={closeHandler}
+              />
+            </div>
+            <input
+              type="text"
+              placeholder="New Task..."
+              className="h-8 w-3/4 md:w-1/2 placeholder:p-2 mt-auto mb-5"
+              onChange={handleChange}
+            />
+            <div className="p-5 w-3/4 flex justify-around text-sm md:text-base">
+              <button
+                className="bg-gradient-to-br from-[#f8d49a] to-[#f1b95f] p-2"
+                onClick={addHandler}
+              >
+                Add
+              </button>
+              <button
+                className="bg-gradient-to-br from-[#f8d49a] to-[#f1b95f] p-2"
+                onClick={closeHandler}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </Fragment>
+  );
+}
+
+export default AddTaskModal;
--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,18 +1,29 @@
+import { useSelector, useDispatch } from "react-redux";
+import { modalActions } from "../store/modal";
+
 import { RiAddBoxFill } from "react-icons/ri";
 
 function Day(props) {
+  const plans = useSelector((state) => state.plan.saturday);
+  const dispatch = useDispatch();
+
+  const addTaskHandler = () => {
+    dispatch(modalActions.setOpen(true));
+  };
   return (
     <div className="bg-[#fcf8f0] h-[200px] rounded-md relative my-2 md:h-[300px] shadow-md">
-      <h1 className="h-max py-1 text-[#d4993b] font-bold text-xl text-center rounded-t-md bg-gradient-to-br from-[#fce0b3] to-[#f5e2c4]">
+      <h1 className="first-letter:capitalize h-max py-1 text-[#d4993b] font-bold text-xl text-center rounded-t-md bg-gradient-to-br from-[#fce0b3] to-[#f5e2c4]">
         {props.day}
       </h1>
       <section className="p-3 mx-2 text-lg list-decimal">
-        <li>task1</li>
-        <li>task2</li>
+        {plans.map((task) => (
+          <li key={task}>{task}</li>
+        ))}
       </section>
       <RiAddBoxFill
         size={"1.75rem"}
         className="absolute bottom-[10px] right-[10px] cursor-pointer active:text-[#f0ae51] md:hover:text-[#f0ae51] md:active:text-[#b6833c]"
+        onClick={addTaskHandler}
       />
     </div>
   );
